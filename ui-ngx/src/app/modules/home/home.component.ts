99b859f4709e85384cde288149dbe6f7e4a749b2
dwbeziek <dwbeziek@gmail.com>
2025-03-06 14:27:35 +0200

    *initial commit with cryolytix logo

--- ui-ngx/src/app/modules/home/home.component.ts
+++ ui-ngx/src/app/modules/home/home.component.ts
@@ -52,7 +52,7 @@ export class HomeComponent extends PageComponent implements AfterViewInit, OnIni
   sidenavMode: 'over' | 'push' | 'side' = 'side';
   sidenavOpened = true;
 
-  logo = 'assets/logo_title_white.svg';
+  logo = 'assets/logo_cryolytix_title_white.svg';
 
   @ViewChild('sidenav')
   sidenav: MatSidenav;
